72e50ef85ea80de69b3c2cb87636e345d16a4532
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2021-10-21 04:07:35 +0100

    (feat): updated entry files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+/* -------------------------------------------------------------------------- */
+/*            Author: Adenekan Wonderful (https://codewonders.dev)            */
+/* -------------------------------------------------------------------------- */
+
 export * from './scoutbar';
 export * from './helpers/action-helpers';
 export * from './helpers/context';
-export * from './helpers/types';
 
 export { default as useScoutKey } from './use-scout-key';
 export { default as useScoutShortcut } from './use-scout-shortcut';
@@ -10,11 +13,12 @@ export { default as ScoutbarInput } from './components/input';
 export { default as ScoutBarStem } from './components/stem';
 export { default as ScoutBar } from './scoutbar';
 
-export {
-  createScoutAction,
-  createScoutActionSection,
-} from './helpers/action-helpers';
-
-// Private Methods
+/* ----------------------------- Private Methods ---------------------------- */
+/**
+ * @remarks These methods are privately used in the application
+ * but are exported and can be used by other applications.
+ */
 export { default as useTrapFocus } from './helpers/use-trap-focus';
 export { default as useIsMounted } from './helpers/use-is-mounted';
+export { default as useLocalStorage } from './helpers/use-local-storage';
+export { default as useOnClickOutside } from './helpers/use-click-outside';
