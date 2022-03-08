1495189fe698c4b32ea3a9317fc2940f9418dbd1
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2022-03-08 16:46:01 +0100

    (chore): updated Scoutbar Stem

--- src/components/stem/index.tsx
+++ src/components/stem/index.tsx
@@ -2,11 +2,12 @@
 /*                            External Dependencies                           */
 /* -------------------------------------------------------------------------- */
 import React, {
+  Dispatch,
   memo,
+  SetStateAction,
   useCallback,
   useContext,
   useEffect,
-  useLayoutEffect,
   useRef,
   useState,
 } from 'react';
@@ -18,10 +19,11 @@ import {
   IScoutAction,
   IScoutSectionAction,
   ScoutBarProps,
-  useScoutKey,
   useScoutShortcut,
   useLocalStorage,
+  useKeybaordNavigation,
   IScoutStems,
+  IScoutStem,
 } from 'index';
 import Icon from 'components/icon';
 
@@ -29,6 +31,7 @@ import Icon from 'components/icon';
 /* @ts-ignore */
 import styles from './stem.module.scss';
 
+type Cell = HTMLAnchorElement | HTMLButtonElement;
 const ScoutBarStem = ({
   actions,
   brandColor,
@@ -37,109 +40,25 @@ const ScoutBarStem = ({
   const [items, setItems] = useState<
     JSX.Element | JSX.Element[] | IScoutStems | undefined
   >(<></>);
-
-  const [cursor, setCursor] = useState<number>(0);
-  const [hovered, setHovered] = useState<HTMLElement | undefined | null>(
-    undefined
-  );
-  const [recentSearch, removeRecent] = useLocalStorage(
+  const [recentSearch, removeRecent] = useLocalStorage<string[]>(
     'scoutbar:recent-search',
     []
   );
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [cursor, setHovered, allCellElements] = useKeybaordNavigation(ref);
+
   const { currentSection, setInputValue } = useContext(ScoutBarContext);
 
-  /**
-   * Scout Key Stroke Handlers
-   */
-  const downPress = useScoutKey('ArrowDown', true);
-  const upPress = useScoutKey('ArrowUp', true);
-  const enterPress = useScoutKey('Enter', true);
-
-  const ref = useRef(null);
-
-  const isMobile = window?.matchMedia(
-    'only screen and (max-width: 768px)'
-  )?.matches;
-
-  const scrollStemSection = () => {
-    (ref?.current as unknown as HTMLElement).scrollTo({
+  const scrollStemSection = useCallback(() => {
+    ref?.current?.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth',
     });
-  };
-
-  /**
-   * Check if we are on mobile device and
-   * disable fetching all actions
-   */
-  const allActions: HTMLElement[] =
-    (!isMobile &&
-      Array.from(
-        !isEmpty(ref?.current)
-          ? (ref?.current as unknown as HTMLElement)?.querySelectorAll(
-              'a, button'
-            )
-          : []
-      )) ||
-    [];
-  const elementActive = allActions[cursor];
-
-  useLayoutEffect(() => {
-    // Get original body overflow
-    const originalStyle = window.getComputedStyle(document.body).overflow;
-    if (upPress || downPress || isMobile) {
-      // Prevent scrolling on mount or deps check
-      document.body.style.overflow = 'hidden';
-    }
-
-    return () => {
-      // Re-enable scrolling when component unmounts
-      document.body.style.overflow = originalStyle;
-    };
-  }, [upPress, downPress, isMobile]);
-
-  useEffect(() => {
-    if (elementActive && (upPress || downPress)) {
-      /**
-       * Allow elements scroll into view on keydown
-       */
-      elementActive?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'nearest',
-      });
-    }
-    if (elementActive && enterPress) {
-      /**
-       * initial focus on enter
-       */
-      elementActive?.click();
-    }
-  }, [upPress, downPress, cursor, enterPress]);
-
-  useEffect(() => {
-    if (allActions?.length && downPress) {
-      setCursor(prevState =>
-        prevState < allActions.length - 1 ? prevState + 1 : prevState
-      );
-    }
-  }, [downPress]);
-
-  useEffect(() => {
-    if (allActions?.length && upPress) {
-      setCursor(prevState => (prevState > 0 ? prevState - 1 : prevState));
-    }
-  }, [upPress]);
-
-  useEffect(() => {
-    if (allActions?.length && hovered) {
-      setCursor(allActions.indexOf(hovered));
-    }
-  }, [hovered]);
+  }, []);
 
   const createScoutbarStemItems = useCallback(
-    (item: IScoutStems | any) => {
+    (item: IScoutStem | any) => {
       if (
         item.type === 'scout-action' ||
         (item.type === 'scout-section-page' &&
@@ -151,9 +70,9 @@ const ScoutBarStem = ({
           <ScoutbarStemCell
             item={item}
             key={item.id}
-            setHovered={setHovered}
             active={cursor}
-            allActions={allActions}
+            setHovered={setHovered}
+            allCellElements={allCellElements}
             scrollStemSection={scrollStemSection}
           />
         );
@@ -191,7 +110,7 @@ const ScoutBarStem = ({
   }, [createScoutbarStemItems, actions]);
 
   const removeRecentSearch = (value: string) => {
-    (removeRecent as Function)?.((prev: string[]) => {
+    removeRecent?.((prev: string[]) => {
       return prev.filter((item: string) => item !== value);
     });
   };
@@ -229,7 +148,7 @@ const ScoutBarStem = ({
           <>
             {showRecentSearch && (
               <RecentSearch
-                recents={recentSearch as string[]}
+                recents={recentSearch}
                 removeRecentSearch={removeRecentSearch}
                 setInputValue={setInputValue}
               />
@@ -244,136 +163,140 @@ const ScoutBarStem = ({
 
 const ScoutbarStemCell: React.FC<{
   item: IScoutAction | IScoutSectionAction;
-  setHovered: (hover: HTMLElement | undefined | null) => void;
   active: number;
   scrollStemSection: () => void;
-  allActions: HTMLElement[];
-}> = memo(({ item, setHovered, active, allActions, scrollStemSection }) => {
-  const isNewPage =
-    item.type === 'scout-section-page' && item?.children?.length > 0;
-  const { setCurrentSection } = useContext(ScoutBarContext);
+  setHovered: Dispatch<SetStateAction<Cell | null | undefined>>;
+  allCellElements: Cell[];
+}> = memo(
+  ({ item, active, setHovered, allCellElements, scrollStemSection }) => {
+    const isNewPage =
+      item.type === 'scout-section-page' && item?.children?.length > 0;
+    const { setCurrentSection } = useContext(ScoutBarContext);
 
-  const ref = useRef(null);
-  const elementActive =
-    allActions.indexOf(ref?.current as unknown as HTMLElement) === active;
+    const ref = useRef(null);
+    const elementActive =
+      active >= 0 && ref?.current
+        ? allCellElements.indexOf(ref?.current) === active
+        : false;
 
-  const setSection = useCallback(
-    item => {
-      setCurrentSection?.(item);
+    const setSection = useCallback(
+      item => {
+        setCurrentSection?.(item);
+        /**
+         * Make sure sections starts from the top of the bar
+         */
+        scrollStemSection();
+      },
+      [scrollStemSection]
+    );
+
+    const handleClick: React.MouseEventHandler<
+      HTMLButtonElement | HTMLAnchorElement
+    > = useCallback(e => {
+      if (isNewPage) return setSection(item);
+
+      item.action?.call(e);
+    }, []);
+
+    const handleShortcutAction: React.MouseEventHandler<
+      HTMLButtonElement | HTMLAnchorElement
+    > = useCallback(e => {
       /**
-       * Make sure sections starts from the top of the bar
+       * Make the element active has a click event that matches expected behaviour
        */
-      scrollStemSection();
-    },
-    [scrollStemSection]
-  );
 
-  const handleClick: React.MouseEventHandler<
-    HTMLButtonElement | HTMLAnchorElement
-  > = useCallback(e => {
-    if (isNewPage) return setSection(item);
+      if (isNewPage) return setSection(item);
+      if (item.href && !item?.target) return window.location.assign(item?.href);
+      if (item.href && item?.target)
+        return window.open(item?.href, item?.target);
 
-    item.action?.call(e);
-  }, []);
+      item.action?.call(e);
+    }, []);
 
-  const handleShortcutAction: React.MouseEventHandler<
-    HTMLButtonElement | HTMLAnchorElement
-  > = useCallback(e => {
-    /**
-     * Make the element active has a click event that matches expected behaviour
-     */
+    const keyboardShortcut =
+      (item.type === 'scout-action' && item.keyboardShortcut) || [];
 
-    if (isNewPage) return setSection(item);
-    if (item.href && !item?.target) return window.location.assign(item?.href);
-    if (item.href && item?.target) return window.open(item?.href, item?.target);
+    if (keyboardShortcut.length > 0) {
+      useScoutShortcut([...keyboardShortcut], handleShortcutAction, {
+        override: true,
+      });
+    }
 
-    item.action?.call(e);
-  }, []);
-
-  const keyboardShortcut =
-    (item.type === 'scout-action' && item.keyboardShortcut) || [];
-
-  if (keyboardShortcut.length > 0) {
-    useScoutShortcut([...keyboardShortcut], handleShortcutAction, {
-      override: true,
-    });
-  }
-
-  const CommonElement = () => (
-    <>
-      {typeof item.icon === 'string' ? (
-        <img src={item.icon} alt={item?.label || ''} aria-hidden="true" />
-      ) : (
-        <>{item.icon}</>
-      )}
-
-      <p>
-        {item.label}
-        {item.description && (
-          <span className={styles.description}>{item.description}</span>
+    const CellCommonElement = () => (
+      <>
+        {typeof item.icon === 'string' ? (
+          <img src={item.icon} alt={item?.label || ''} aria-hidden="true" />
+        ) : (
+          <>{item.icon}</>
         )}
-        {keyboardShortcut.length > 0 && (
-          <span className={styles.shortcut}>
-            {keyboardShortcut?.map((key: string) => (
-              <span key={key}>{key}</span>
-            ))}
-          </span>
-        )}
+        <p>
+          {item.label}
+          {item.description && (
+            <span className={styles.description}>{item.description}</span>
+          )}
+          {keyboardShortcut.length > 0 && (
+            <span className={styles.shortcut}>
+              {keyboardShortcut?.map((key: string) => (
+                <span key={key}>{key}</span>
+              ))}
+            </span>
+          )}
+          {isNewPage && (
+            <Icon
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              className={styles.newPageIcon}
+              aria-hidden="true"
+            >
+              <path
+                d="M0 14C0 14.5304 0.210714 15.0391 0.585786 15.4142C0.960859 15.7893 1.46957 16 2 16H14C14.5304 16 15.0391 15.7893 15.4142 15.4142C15.7893 15.0391 16 14.5304 16 14V2C16 1.46957 15.7893 0.960859 15.4142 0.585786C15.0391 0.210714 14.5304 0 14 0L2 0C1.46957 0 0.960859 0.210714 0.585786 0.585786C0.210714 0.960859 0 1.46957 0 2L0 14ZM4.5 7.5H10.293L8.146 5.354C8.05211 5.26011 7.99937 5.13278 7.99937 5C7.99937 4.86722 8.05211 4.73989 8.146 4.646C8.23989 4.55211 8.36722 4.49937 8.5 4.49937C8.63278 4.49937 8.76011 4.55211 8.854 4.646L11.854 7.646C11.9006 7.69245 11.9375 7.74762 11.9627 7.80837C11.9879 7.86911 12.0009 7.93423 12.0009 8C12.0009 8.06577 11.9879 8.13089 11.9627 8.19163C11.9375 8.25238 11.9006 8.30755 11.854 8.354L8.854 11.354C8.76011 11.4479 8.63278 11.5006 8.5 11.5006C8.36722 11.5006 8.23989 11.4479 8.146 11.354C8.05211 11.2601 7.99937 11.1328 7.99937 11C7.99937 10.8672 8.05211 10.7399 8.146 10.646L10.293 8.5H4.5C4.36739 8.5 4.24021 8.44732 4.14645 8.35355C4.05268 8.25979 4 8.13261 4 8C4 7.86739 4.05268 7.74021 4.14645 7.64645C4.24021 7.55268 4.36739 7.5 4.5 7.5V7.5Z"
+                fill="black"
+              />
+            </Icon>
+          )}
+        </p>
+      </>
+    );
 
-        {isNewPage && (
-          <Icon
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            className={styles.newPageIcon}
-            aria-hidden="true"
+    const cellCommonProps = {
+      onClick: handleClick,
+      onMouseEnter: () => setHovered(ref?.current),
+      onMouseLeave: () => setHovered(undefined),
+      className: classNames([
+        'scoutbar-cell-item',
+        styles.scoutbarStemCell,
+        `${elementActive && styles.active}`,
+      ]),
+      'aria-label': item.ariaLabel,
+      ref,
+    };
+
+    return (
+      <>
+        {item.href ? (
+          <a
+            href={item.href}
+            {...cellCommonProps}
+            target={item?.target}
+            rel={item?.rel}
           >
-            <path
-              d="M0 14C0 14.5304 0.210714 15.0391 0.585786 15.4142C0.960859 15.7893 1.46957 16 2 16H14C14.5304 16 15.0391 15.7893 15.4142 15.4142C15.7893 15.0391 16 14.5304 16 14V2C16 1.46957 15.7893 0.960859 15.4142 0.585786C15.0391 0.210714 14.5304 0 14 0L2 0C1.46957 0 0.960859 0.210714 0.585786 0.585786C0.210714 0.960859 0 1.46957 0 2L0 14ZM4.5 7.5H10.293L8.146 5.354C8.05211 5.26011 7.99937 5.13278 7.99937 5C7.99937 4.86722 8.05211 4.73989 8.146 4.646C8.23989 4.55211 8.36722 4.49937 8.5 4.49937C8.63278 4.49937 8.76011 4.55211 8.854 4.646L11.854 7.646C11.9006 7.69245 11.9375 7.74762 11.9627 7.80837C11.9879 7.86911 12.0009 7.93423 12.0009 8C12.0009 8.06577 11.9879 8.13089 11.9627 8.19163C11.9375 8.25238 11.9006 8.30755 11.854 8.354L8.854 11.354C8.76011 11.4479 8.63278 11.5006 8.5 11.5006C8.36722 11.5006 8.23989 11.4479 8.146 11.354C8.05211 11.2601 7.99937 11.1328 7.99937 11C7.99937 10.8672 8.05211 10.7399 8.146 10.646L10.293 8.5H4.5C4.36739 8.5 4.24021 8.44732 4.14645 8.35355C4.05268 8.25979 4 8.13261 4 8C4 7.86739 4.05268 7.74021 4.14645 7.64645C4.24021 7.55268 4.36739 7.5 4.5 7.5V7.5Z"
-              fill="black"
-            />
-          </Icon>
+            <CellCommonElement />
+          </a>
+        ) : (
+          <button
+            type="button"
+            {...cellCommonProps}
+            disabled={!item.action && item.type === 'scout-action'}
+            aria-hidden={!item.action && item.type === 'scout-action'}
+          >
+            <CellCommonElement />
+          </button>
         )}
-      </p>
-    </>
-  );
-
-  const commonProps = {
-    onClick: handleClick,
-    onMouseEnter: () => setHovered(ref?.current),
-    onMouseLeave: () => setHovered(undefined),
-    className: classNames([
-      styles.scoutbarStemCell,
-      `${elementActive && styles.active}`,
-    ]),
-    'aria-label': item.ariaLabel,
-    ref,
-  };
-
-  return (
-    <>
-      {item.href ? (
-        <a
-          href={item.href}
-          {...commonProps}
-          target={item?.target}
-          rel={item?.rel}
-        >
-          <CommonElement />
-        </a>
-      ) : (
-        <button
-          type="button"
-          {...commonProps}
-          disabled={!item.action && item.type === 'scout-action'}
-          aria-hidden={!item.action && item.type === 'scout-action'}
-        >
-          <CommonElement />
-        </button>
-      )}
-    </>
-  );
-});
+      </>
+    );
+  }
+);
 
 const ScoutbarStemItem: React.FC<{
   item: IScoutSectionAction;
@@ -428,7 +351,10 @@ const RecentSearch: React.FC<{
                     />
                   </Icon>
                   <button
-                    className={styles.recentButton}
+                    className={classNames([
+                      styles.recentButton,
+                      'scoutbar-recent-button',
+                    ])}
                     onClick={() => setInputValue?.(search)}
                   >
                     {search}
@@ -481,4 +407,12 @@ const RecentSearch: React.FC<{
   );
 });
 
+/**
+ * Set display name for the component so its easily debuggable.
+ * the memo method prevents the component
+ * from re-rendering when the props change and renames it as Anonymous
+ */
+RecentSearch.displayName = 'RecentSearch';
+ScoutbarStemItem.displayName = 'ScoutbarStemItem';
+ScoutbarStemCell.displayName = 'ScoutbarStemCell';
 export default ScoutBarStem;
